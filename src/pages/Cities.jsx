0fc533cbaa098ed9dd3add37ea1876106658eb5f
Graciela Romano <76084759+GracielaRomano@users.noreply.github.com>
2023-08-04 23:39:25 -0300

    Carousel funcionando (#2)

---
+++ src/pages/Cities.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../layouts/NavBar"
+import Footer from "../components/Footer"
+
+export default function Cities() {
+  return (
+    <>
+        <NavBar />
+        <div>
+            <h1>Page under construction</h1>
+        </div>
+        <Footer />
+    </>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,24 @@ export default function Home() {
   return (
     <>
     <NavBar />
-    <div className="contenedor-home">
+    <div className="container-home">
         <div className="row">
-            <div className="col-7">
-                <h1>aqui va la card</h1>
-            </div>
-            <div className="col-5" >
-              <div className="contenedor">
-                <Carousel/>
+            <div className="col-sm-12 col-lg-7">
+            <div className="card card-home">
+              <div className="card-header card-header-home">
+              Find the perfect destination
               </div>
+              <div className="card-body">
+                <p className="card-text card-text-home">Our app will help you find the perfect path for your next trip. With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
+                <p className="text-slogan">"Find your perfect trip, designed by insiders who know and love their cities!"</p>
+                <a href="#" className="btn btn-card-home">View More</a>
+              </div>
+            </div>
+            </div>
+            <div className="col-sm-12 col-lg-5" >
+              <h3 className="titulo-carousel">"Popular Mytineraries"</h3>
+                <Carousel/>
+              
             </div>
           </div>
     </div>
@@ -22,41 +31,3 @@ export default function Home() {
   )
 }
 
-{/*<div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="/img/europe/ibiza.jpg" className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>*/}
