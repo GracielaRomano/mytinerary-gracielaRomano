37b545660e956aa4e629c9d9c80f99decca3642d
Graciela Romano <graci.romano@gmail.com>
2023-08-26 20:28:00 -0300

    Se crea el reductor de la page cities

--- src/store/actions/cities.jsx
+++ src/store/actions/cities.jsx
@@ -7,7 +7,7 @@ const read_carousel = createAsyncThunk(
     async()=> {         //callback que realiza la petición
         try {
             let data = await axios(apiUrl+'cities/carousel')
-            console.log(data);
+            //console.log(data);
             return { carousel: data.data.data_carousel } //data_carousel viene del back
         } catch (err) {
             console.log(err);
@@ -17,6 +17,21 @@ const read_carousel = createAsyncThunk(
         }
     }
 )
+const read_cities = createAsyncThunk(
+    'read_cities',
+    async(obj)=> {
+        try{
+            let data =await axios(apiUrl+'cities?city='+obj.text)
+            console.log('dato recibido desde axios: ',data)
+            return{
+                cities: data.data.response
+            }
+        }catch(err){
+            console.log(err)
+            throw err; // Lanzar el error para que pueda ser manejado en el reducer
+        }
+    }
+)
 
-const cities_actions = {read_carousel}
+const cities_actions = {read_carousel, read_cities}
 export default cities_actions
--- src/store/reducers/cities.jsx
+++ src/store/reducers/cities.jsx
@@ -1,10 +1,12 @@
 import { createReducer } from "@reduxjs/toolkit";
 import cities_actions from "../actions/cities";
 
-const { read_carousel} = cities_actions
+const { read_carousel, read_cities} = cities_actions
 
 const initial_state = {
-    carousel: []
+    carousel: [],
+    cities: [],
+    resultNotFound: false // Agregar el estado para controlar si no se encontraron resultados
 }
 
 const cities_reducer = createReducer(
@@ -19,6 +21,26 @@ const cities_reducer = createReducer(
             return new_state
         }
     )
-    
+    .addCase(
+       read_cities.fulfilled,
+       (state,action)=> {
+        let new_state = {
+            ...state,
+            cities: action.payload.cities,
+            resultNotFound: action.payload.cities.length === 0 // Actualizar el estado según la longitud de las ciudades
+        }
+        return new_state
+       } 
+    )
+    .addCase(
+        read_cities.rejected, // Agregar caso para manejar el error
+        (state, action) => {
+            return {
+                ...state,
+                cities: [], // Restablecer las ciudades a una lista vacía
+                resultNotFound: true // Establecer el estado como verdadero al producirse un error
+            };
+        }
+    )
 )
 export default cities_reducer
