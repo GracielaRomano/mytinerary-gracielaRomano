9176fe17e735d8a4bf2875ffe9350e687b713971
Graciela Romano <graci.romano@gmail.com>
2023-09-03 20:42:25 -0300

    Se coloca un evento en el icono de like

--- src/components/CardItineraries.jsx
+++ src/components/CardItineraries.jsx
@@ -7,13 +7,22 @@ export default function CardItineraries({ title, imageSrc, favoriteCount, conten
   const toggleExpand = () => {
     setExpanded(!expanded);
   }; 
+
+  const [like, setLike] =useState(0);
+  const [isLike, setIsLike] = useState(false)
+  const onLikeButton = () => {
+    setLike(like + (isLike ? -1 :1));
+    setIsLike(!isLike);
+  }
   return (
     <div className={`card-itineraries ${expanded ? 'expanded' : ''}`}>
       <h3 className='title-card-itineraries'>{title}</h3>
       <img className="card-image" src={imageSrc} alt="Card" />
       <div className="card-content">
         <div className="favorite-count">
-          <i className="bi bi-heart-fill"></i>{favoriteCount}
+          <p className={'' + (isLike ? "text violet":"")}>
+          <i  className="bi bi-heart-fill " onClick={onLikeButton}/>{favoriteCount} {like}
+          </p>
         </div>
         <div className="expand-button" onClick={toggleExpand}>
           <i className={`arrow ${expanded ? 'bi bi-caret-up-fill' : 'bi bi-caret-down-fill'}`}></i>
