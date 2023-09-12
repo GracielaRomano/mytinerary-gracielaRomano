3fd48a6b816616e8a5482f526009702bfe857d6b
Graciela Romano <graci.romano@gmail.com>
2023-09-12 11:12:33 -0300

    Algunos cambios en el componente activities para utilizar dispach

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,24 +1,19 @@
 import CardActivities from "./CardActivities";
 import { useState, useEffect } from "react";
 import activity_actions from "../store/actions/activities";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
-import apiUrl from "../apiUrl";
+import { useDispatch } from "react-redux";
+
 
 const { read_activities } = activity_actions;
 
 export default function Activities({ p_activities }) {
-   const [activities, setActivities] = useState([]);
-  
-   //const dispatch = useDispatch();
-   
+   const [activities, setActivity] = useState([]);
+   const dispatch = useDispatch();
 
    useEffect(() => {
-     //dispatch(read_activities({ itinerary_id: p_activities }));
-      
-      axios(apiUrl +'activities?itinerary_id='+p_activities)
-      .then(res => setActivities(res.data.response))
-      .catch(err=> console.log(err))
+     dispatch(read_activities({ itinerary_id: p_activities }))
+     .then(response => setActivity(response.payload.activities))
+     .catch(err => console.log(err))
    }, []);
    //const activities = useSelector((store) => store.activities.activities);
   return (
